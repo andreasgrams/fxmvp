c3ad54ebd8df06f8822d299426e9c6e137c2146e
Andreas Grams <andreas.grams@gmail.com>
2016-03-12 22:31:10 +0100

    new constructor

--- src/main/java/de/dreamnetworx/fxmvp/base/FxMvpException.java
+++ src/main/java/de/dreamnetworx/fxmvp/base/FxMvpException.java
@@ -2,6 +2,10 @@ package de.dreamnetworx.fxmvp.base;
 
 public class FxMvpException extends RuntimeException {
 
+    public FxMvpException(final String messgae) {
+        super(messgae);
+    }
+
     public FxMvpException(final String messgae, final Throwable t) {
         super(messgae, t);
     }
